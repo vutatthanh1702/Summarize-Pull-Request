73389825eba7b119d95d2c8245d8f5f1e6cde393
vutatthanh1702 <39393560+vutatthanh1702@users.noreply.github.com>
2023-08-14 11:27:21 +0900

    Update github-pr-summary.rs
    fix

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -205,7 +205,7 @@ async fn handler(
             restart: true,
             system_prompt: Some(system),
         };
-        let question = "Here is a set of summaries for software source code patches. Each summary starts with a ------ line. Please write an overall summary considering all the individual summary. Please present the potential issues and errors first, following by the most important findings, in your summary. Next, Please present the ways to achieve the 5 five SOLID principles following by the most important findings, in your summary (Single-responsibility principle, Open-closed principle, Liskov substitution principle, Interface segregation principle, Dependency inversion principle) in this.\n\n".to_string() + &reviews_text;
+        let question = "Here is a set of summaries for software source code patches. Each summary starts with a ------ line. Please write an overall summary considering all the individual summary. Please present the potential issues and errors first, and the ways to achieve the 5 five SOLID principles following by the most important findings, in your summary.\n\n".to_string() + &reviews_text;
         match openai.chat_completion(&chat_id, &question, &co).await {
             Ok(r) => {
                 resp.push_str(&r.choice);
