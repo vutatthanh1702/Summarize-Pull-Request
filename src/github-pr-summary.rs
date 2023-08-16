083a2763965609a97627d1002262940d328d2fc6
vutatthanh1702 <39393560+vutatthanh1702@users.noreply.github.com>
2023-08-16 12:39:24 +0900

    Update github-pr-summary.rs

--- src/github-pr-summary.rs
+++ src/github-pr-summary.rs
@@ -175,7 +175,7 @@ async fn handler(
             restart: true,
             system_prompt: Some(system),
         };
-        let question = "The following is a GitHub patch. Please summarize the key changes and identify potential problems and the ways to achieve the 5 five SOLID principles. Start with the most important findings. write 'look good' if the code don't have serious problem like just 1 line small change that show text in interface.\n\n".to_string() + truncate(commit, CHAR_SOFT_LIMIT);
+        let question = "The following is a GitHub patch. Please summarize the key changes and identify potential problems and the ways to achieve the 5 five SOLID principles. Start with the most important findings.\n\n".to_string() + truncate(commit, CHAR_SOFT_LIMIT);
         match openai.chat_completion(&chat_id, &question, &co).await {
             Ok(r) => {
                 if reviews_text.len() < CHAR_SOFT_LIMIT {
